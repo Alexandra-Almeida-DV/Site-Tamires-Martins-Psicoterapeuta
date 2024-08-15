eda9c299722be77cb67e1085f61aae3dd23e26b9
Alexandra-Almeida-DV <almeidaalexandra04@gmail.com>
2024-08-15 02:16:55 -0300

    factor: criando theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App, { theme } from './App';
-import { CssBaseline, ThemeProvider } from '@mui/material';
+import { theme } from './themes/themes';
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
